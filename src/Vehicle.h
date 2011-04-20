49daf88c1d8553178f3a3c4d9070294eb963ec9c
Chris Tandiono <christandiono@berkeley.edu>
2011-04-19 18:49:09 -0700

    test

---
+++ src/Vehicle.h
@@ -0,0 +1,9 @@
+// vim: ts=2:sw=2:softtabstop=2
+
+class Vehicle {
+  public:
+    void accelerate();
+    void decelerate();
+    void draw(mat4 rotation); // handles translation on its own
+  private:
+};
--- src/sweep.cpp
+++ src/sweep.cpp
@@ -1,3 +1,4 @@
+// vim: ts=2:sw=2:softtabstop=2
 #include "sweep.h"
 
 #include "global.h"
